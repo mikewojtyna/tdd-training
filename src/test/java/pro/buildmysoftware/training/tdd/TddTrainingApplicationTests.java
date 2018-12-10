1c6735c8060f482e73071da7b6bc702ab86ddbcd
Mike Wojtyna <mike@buildmysoftware.pro>
2018-12-10 23:18:56 +0100

    Init repository

---
+++ src/test/java/pro/buildmysoftware/training/tdd/TddTrainingApplicationTests.java
@@ -0,0 +1,18 @@
+package pro.buildmysoftware.training.tdd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TddTrainingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
