babd197661d28453c25c3525f34e61276bfd1246
Mike Wojtyna <mike@buildmysoftware.pro>
2018-12-19 15:39:34 +0100

    Finish stack overflow question kata

--- src/main/java/pro/buildmysoftware/training/tdd/kata/Question.java
+++ src/main/java/pro/buildmysoftware/training/tdd/kata/Question.java
@@ -1,29 +1,43 @@
 package pro.buildmysoftware.training.tdd.kata;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Question {
 	private int score;
 	private User author;
-	private Collection<User> voters;
+	// private Collection<User> voters;
+	private Map<User, Integer> voters;
 
 	public Question(int score, User author) {
 		this.score = score;
 		this.author = author;
-		voters = new ArrayList<>();
+		voters = new HashMap<>();
 	}
 
 	public void upvoteBy(User user) {
-		boolean userIsDifferentThanAuthor = !author.equals(user);
-		boolean userAlreadyVoted = voters.contains(user);
-		if (userIsDifferentThanAuthor && !userAlreadyVoted) {
-			score++;
-			voters.add(user);
-		}
+		changeScoreBy(1, user);
 	}
 
 	public int getScore() {
 		return score;
+	}
+
+	public void downvoteBy(User user) {
+		changeScoreBy(-1, user);
+	}
+
+	private void changeScoreBy(int scoreChange, User user) {
+		boolean userIsDifferentThanAuthor = !author.equals(user);
+		Integer oldUserScore = voters.getOrDefault(user, 0);
+		int scoreAfterChange = oldUserScore + scoreChange;
+		if (isVoteAllowed(scoreAfterChange) && userIsDifferentThanAuthor) {
+			score += scoreChange;
+			voters.put(user, scoreAfterChange);
+		}
+	}
+
+	private boolean isVoteAllowed(int scoreAfterChange) {
+		return scoreAfterChange > -2 && scoreAfterChange < 2;
 	}
 }
--- src/test/java/pro/buildmysoftware/training/tdd/kata/QuestionTest.java
+++ src/test/java/pro/buildmysoftware/training/tdd/kata/QuestionTest.java
@@ -8,12 +8,148 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class QuestionTest {
 
+	// @formatter:off
+	@DisplayName(
+		"given question with initial score 0, " +
+		"when upvote the question and downvote it later by " +
+		"the same user, " +
+		"then the score should remain 0"
+	)
+	// @formatter:on
+	@Test
+	void test0() throws Exception {
+		// given
+		Question question = questionWithScoreAndAuthor(0, "author");
+		User user = userWithName("goobar");
+
+		// when
+		question.upvoteBy(user);
+		question.downvoteBy(user);
+
+		// then
+		assertThat(question.getScore()).isZero();
+	}
+
+	// @formatter:off
+	@DisplayName(
+		"given question with high score, " +
+		"then voting should behave as expected")
+	// @formatter:on
+	@Test
+	void test1() throws Exception {
+		// given
+		Question question = questionWithScoreAndAuthor(100, "mike");
+		User goobar = userWithName("goobar");
+		User foobar = userWithName("foobar");
+		User hoobar = userWithName("hoobar");
+
+		// when
+		question.upvoteBy(goobar);
+		question.upvoteBy(goobar);
+		question.upvoteBy(foobar);
+		question.upvoteBy(foobar);
+		question.downvoteBy(foobar);
+		question.downvoteBy(hoobar);
+		question.upvoteBy(hoobar);
+		// expected result: 101
+
+		// then
+		assertThat(question.getScore()).isEqualTo(101);
+	}
+
 	private Question questionWithScoreAndAuthor(int score, String author) {
 		return new Question(score, userWithName(author));
 	}
 
 	private User userWithName(String user) {
 		return new User(user);
+	}
+
+	@DisplayName("downvote examples")
+	@Nested
+	class Downvote {
+		// @formatter:off
+		@DisplayName(
+			"given question with initial score 0, " +
+			"when downvote the question by user other than author, " +
+			"then score is -1")
+		// @formatter:on
+		@Test
+		void test0() throws Exception {
+			// given
+			Question question = questionWithScoreAndAuthor(0,
+				"author");
+			User foobar = userWithName("foobar");
+
+			// when
+			question.downvoteBy(foobar);
+
+			// then
+			assertThat(question.getScore()).isEqualTo(-1);
+		}
+
+		// @formatter:off
+		@DisplayName(
+			"given question with initial score 0, " +
+			"when downvote the question twice by user other than author, " +
+			"then score is -1")
+		// @formatter:on
+		@Test
+		void test1() throws Exception {
+			// given
+			Question question = questionWithScoreAndAuthor(0,
+				"author");
+			User user = userWithName("foobar");
+
+			// when
+			question.downvoteBy(user);
+			question.downvoteBy(user);
+
+			// then
+			assertThat(question.getScore()).isEqualTo(-1);
+		}
+
+		// @formatter:off
+		@DisplayName(
+			"given question with initial score 0, " +
+			"when downvote the question twice by author, " +
+			"then score is 0")
+		// @formatter:on
+		@Test
+		void test2() throws Exception {
+			// given
+			Question question = questionWithScoreAndAuthor(0,
+				"author");
+			User author = userWithName("author");
+
+			// when
+			question.downvoteBy(author);
+
+			// then
+			assertThat(question.getScore()).isEqualTo(0);
+		}
+
+		// @formatter:off
+		@DisplayName(
+			"given question with initial score 0, " +
+			"when downvote the question by 2 authors, " +
+			"then score is -2")
+		// @formatter:on
+		@Test
+		void test3() throws Exception {
+			// given
+			Question question = questionWithScoreAndAuthor(0,
+				"author");
+			User foobar = userWithName("foobar");
+			User goobar = userWithName("goobar");
+
+			// when
+			question.downvoteBy(foobar);
+			question.downvoteBy(goobar);
+
+			// then
+			assertThat(question.getScore()).isEqualTo(-2);
+		}
 	}
 
 	@DisplayName("upvote examples")
